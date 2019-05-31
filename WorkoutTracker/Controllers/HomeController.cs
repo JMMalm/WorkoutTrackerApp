e66aa9b3e3f5317a6df8981c336bc3a03979ad32
Jordon Malm <jordon.malm@gmail.com>
2019-05-30 23:49:14 -0500

    Update Home controller's Index action and add boilerplate Details action. Add temporary dummy data to expedite creation of the UI. Add *.Core library and Workout class. Update gitignore and .editorconfig.

--- WorkoutTracker/Controllers/HomeController.cs
+++ WorkoutTracker/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorkoutTracker.Core;
 using WorkoutTracker.Models;
 
 namespace WorkoutTracker.Controllers
@@ -12,7 +13,12 @@ namespace WorkoutTracker.Controllers
 	{
 		public IActionResult Index()
 		{
-			return View();
+			return View(GetDummyWorkouts());
+		}
+
+		public IActionResult Details(int id)
+		{
+			return View(GetDummyWorkouts().Where(w => w.Id == id).FirstOrDefault());
 		}
 
 		public IActionResult Privacy()
@@ -24,6 +30,22 @@ namespace WorkoutTracker.Controllers
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		/// <summary>
+		/// Returns a collection of dummy, in-memory workout data.
+		/// </summary>
+		/// <remarks>
+		/// This is temporary until the database gets set up.
+		/// </remarks>
+		private IEnumerable<Workout> GetDummyWorkouts()
+		{
+			return new List<Workout>
+			{
+				new Workout { Id = 0, Date = DateTime.Now.AddDays(-5), LastModified = DateTime.Now, LastModifiedBy = "jmalm", Location = "Anytime Fitness", UserId = 0 },
+				new Workout { Id = 1, Date = DateTime.Now.AddDays(-3), LastModified = DateTime.Now, LastModifiedBy = "jmalm", Location = "Anytime Fitness", UserId = 0 },
+				new Workout { Id = 2, Date = DateTime.Now.AddDays(-1), LastModified = DateTime.Now, LastModifiedBy = "jmalm", Location = "Anytime Fitness", UserId = 0 }
+			};
 		}
 	}
 }
